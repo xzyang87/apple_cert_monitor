84c77fd5fbbd04de6ef67da5036150253f0c3754
Du Yaowei <d.yaowei@gmail.com>
2018-06-14 15:12:18 +0800

    [Yaowei & Xiaozhu] refactor by print with argument.

--- spec/apple_cert_monitor_spec.rb
+++ spec/apple_cert_monitor_spec.rb
@@ -2,10 +2,11 @@ require_relative '../lib/apple_cert_monitor.rb'
 
 RSpec.describe AppleCertMonitor do
   let(:dummy_class) { Class.new { extend AppleCertMonitor } }
+  let(:msg) { 'Just to print something...' }
 
-  describe ".print_sth" do
-    it "should print to stdout" do
-      expect { dummy_class.print_sth }.to output("Just to print something...").to_stdout
+  describe '.print_sth' do
+    it 'should print to stdout' do
+      expect { dummy_class.print_sth(msg) }.to output(msg).to_stdout
     end
   end
 end
